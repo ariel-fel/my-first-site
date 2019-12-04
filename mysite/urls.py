334fcae361dfcc24d034ce6705ea53aeed808327
Ariel Feldman <feldmanariel@gmail.com>
2019-12-04 09:59:27 +0200

    Added user registeretion app

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('budget/', include('budget.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
